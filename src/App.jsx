143eaf893dd7343ae4fff990e8db5c117a0cd08e
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2024-01-24 13:21:47 +0100

    created meme generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { Header } from './components/Header/header'
-import { MainContent } from './components/Main/mainContent';
-import { Footer } from './components/Footer/footer';
-
+import { Meme } from './components/Main/mainContent'
 import './App.css'
+
 
 export function App() {
  
-
   return (
     <div className='meme-generator'>
       <Header />
-      <MainContent/>
-      <Footer/>
+      <Meme/>
     </div>
   )
 }
+
 
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,8 +2,13 @@ import './header.css'
 
 export function Header(){
   return(
-
-    <h1>header and hei</h1>
-
+    <header className="header">
+      <img 
+          src="./troll-face.png" 
+          className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
   )
 }
